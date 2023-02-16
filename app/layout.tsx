e41247a8acdaaf1378a5b8df13008ec28354c22c
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-16 18:09:36 -0300

    Update home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="pt-BR">
       <head />
-      <body>{children}</body>
+      <body>
+        <header></header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }

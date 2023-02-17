61e286c9caa907dfa72edbb1bb12c58a899a4ad2
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-17 10:34:04 -0300

    Update main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import Header from "./components/header";
 import "./globals.css";
+import { JetBrains_Mono } from "@next/font/google";
+import Header from "./components/header";
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -9,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head />
-      <body className="bg-zinc-900 text-white font-mono">
+      <body className={`bg-zinc-900 text-white ${jetBrainsMono.className}`}>
         <div className="min-w-full min-h-screen flex flex-col items-stretch justify-start">
           <Header />
           <main className="flex-grow flex flex-col items-center justify-center relative">

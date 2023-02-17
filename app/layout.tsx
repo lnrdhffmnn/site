06f011c08342b0abc326e87f190600efe05580ee
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-16 22:37:04 -0300

    Update main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head />
-      <body>
-        <header></header>
-        <main>{children}</main>
+      <body className="bg-zinc-900 text-white font-mono">
+        <div className="min-w-full min-h-screen flex flex-col items-stretch justify-start">
+          <header></header>
+          <main className="flex-grow flex flex-col items-center justify-center relative">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );

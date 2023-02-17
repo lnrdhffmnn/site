c8999b36ce0ff73e414aca9d30804b0ee2aa28ab
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-16 22:43:29 -0300

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <p>Home</p>
+      <Image
+        src="https://github.com/lnrdhffmnn.png"
+        alt="GitHub"
+        width={175}
+        height={175}
+        priority
+        draggable={false}
+        className="rounded-full shadow-sm"
+      />
+      <a
+        href="https://github.com/lnrdhffmnn"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sky-600 font-bold hover:underline mt-2"
+      >
+        @lnrdhffmnn
+      </a>
     </>
   );
 }

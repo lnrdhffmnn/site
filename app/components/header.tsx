f1c67ccea715d60d50364c08446cd0da6e79039b
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-17 11:03:07 -0300

    Update header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 interface MenuItem {
   icon: string;
@@ -20,12 +25,44 @@ export default function Header() {
     },
   ];
 
+  const currentPath = usePathname();
+
+  const [currentHover, setCurrentHover] = useState(currentPath);
+
   return (
-    <header className="w-full flex items-center justify-center gap-8 p-8">
+    <header
+      className="w-full flex items-center justify-center gap-4 p-8 z-50"
+      onMouseLeave={() => setCurrentHover(currentPath)}
+    >
       {menuItems.map(item => (
-        <Link key={item.href} href={item.href} className="flex gap-2">
+        <Link
+          key={item.href}
+          href={item.href}
+          className="flex items-center justify-center gap-2 relative p-2 w-full max-w-[250px]"
+          onMouseEnter={() => setCurrentHover(item.href)}
+        >
           <span>{item.icon}</span>
           <span>{item.label}</span>
+          {currentPath === item.href && (
+            <motion.div
+              layoutId="path"
+              transition={{
+                type: "spring",
+                duration: 0.3,
+              }}
+              className="absolute bottom-0 w-full h-[2px] bg-white rounded"
+            />
+          )}
+          {currentHover === item.href && (
+            <motion.div
+              layoutId="hover"
+              transition={{
+                ease: "easeInOut",
+                duration: 0.2,
+              }}
+              className="absolute bottom-0 w-full h-full bg-zinc-800 rounded-md shadow-sm -z-10"
+            />
+          )}
         </Link>
       ))}
     </header>
